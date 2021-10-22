9c13ade13b57d981e8e92f3319dcb9e539b0e9fd
Genius <Genius@DESKTOP-53Q4II1>
2021-10-22 12:52:58 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace timer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(timer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 2);
+            dispatcherTimer.Start();
+
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            timer.Text = DateTime.Now.ToString("yy MMMM");
+            string path = @"C:\Users\Genius\Desktop\1616585430123.jfif";
+
+            if ( File.Exists((path)))
+
+            {
+                img1.Source = new BitmapImage(new Uri(path));
+
+            }
+            else
+            {
+                MessageBox
+                    .Show("jdj");
+            }
+
+        }
+
+    }
+}
